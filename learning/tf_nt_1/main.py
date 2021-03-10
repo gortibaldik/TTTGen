c556955043304fb6e0049745431a69e5b87dd6b1
gortibaldik <ferotre@gmail.com>
2021-03-10 14:01:46 +0100

    Implement Bahdanau attention and Decoder making use of it

--- learning/tf_nt_1/main.py
+++ learning/tf_nt_1/main.py
@@ -102,12 +102,12 @@ class Encoder(tf.keras.Model):
         return hidden_state
 
 
-class DecoderWA(tf.keras.Model):
-    """ Decoder without attention
+class DecoderNA(tf.keras.Model):
+    """ Decoder no attention
         - computes the predictions from hidden encoder state
     """
     def __init__(self, vocab_size, embedding_dim, n_layers, dec_units, batch_sz):
-        super(DecoderWA, self).__init__()
+        super(DecoderNA, self).__init__()
         self.batch_sz = batch_sz
         self.dec_units = dec_units
         self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
@@ -132,6 +132,60 @@ class DecoderWA(tf.keras.Model):
         return x, hidden_state, None
 
 
+class BahdanauAttention(tf.keras.layers.Layer):
+    def __init__(self, units):
+        super(BahdanauAttention, self).__init__()
+        self.W1 = tf.keras.layers.Dense(units)
+        self.W2 = tf.keras.layers.Dense(units)
+        self.V = tf.keras.layers.Dense(1)
+
+    def call(self, actual_hidden, enc_hidden):
+        # enc_hidden.shape == (BATCH_SIZE, MAXIMUM_SOURCE_SIZE, HIDDEN_SIZE)
+        # actual_hidden.shape == (BATCH_SIZE, HIDDEN_SIZE)
+        # to be able to sum enc_hidden and actual_hidden, we need to
+        # enable distribution over 1st axis, e.g.
+        # actual_hidden.shape must be (BATCH_SIZE, 1, HIDDEN_SIZE)
+        actual_hidden_extended = tf.expand_dims(actual_hidden, 1)
+        score = self.V(tf.nn.tanh(
+                      self.W1(actual_hidden_extended) + self.W2(enc_hidden)))
+        # score.shape == (BATCH_SIZE, MAXIMUM_SOURCE_SIZE, 1)
+        # we want the distribution over MAXIMUM_SOURCE_SIZE
+        alignment_vector = tf.nn.softmax(score, axis=1)
+        context_vector = tf.reduce_sum(alignment_vector * enc_hidden, axis=1)
+
+        return context_vector, alignment_vector
+
+
+class Decoder(tf.keras.Model):
+    """ Decoder no attention
+        - computes the predictions from hidden encoder state
+    """
+    def __init__(self, vocab_size, embedding_dim, n_layers, dec_units, batch_sz):
+        super(Decoder, self).__init__()
+        self.batch_sz = batch_sz
+        self.dec_units = dec_units
+        self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+        self.recurrences = []
+        for _ in range(n_layers):
+            self.recurrences.append(tf.keras.layers.GRU(self.dec_units,
+                                                        return_sequences=True,
+                                                        return_state=True,
+                                                        recurrent_initializer='glorot_uniform'))
+        self.fc = tf.keras.layers.Dense(vocab_size)
+        self.attention = BahdanauAttention(self.dec_units)
+
+    def call(self, x, hidden, enc_output):
+        x = self.embedding(x)
+        context_vector, attention_weights = self.attention(hidden[-1], enc_output)
+        hidden_state = [None] * len(self.recurrences)
+        output = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
+        for l in range(len(self.recurrences)):
+            output, hidden_state[l] = self.recurrences[l](output, initial_state=hidden[l])
+        output = tf.reshape(output, (-1, output.shape[2]))
+        x = self.fc(output)
+        return x, hidden_state, attention_weights
+
+
 def loss_function(x, y, loss_object):
     mask = tf.math.logical_not(tf.math.equal(y, 0))
     loss_ = loss_object(y, x)
@@ -154,7 +208,7 @@ class TrainStepWrapper:
                   , BATCH_SIZE):
         """
         :type encoder: Encoder
-        :type decoder: DecoderWA
+        :type decoder: DecoderNA
         """
         loss = 0
 
@@ -348,11 +402,17 @@ def _main():
     print(f'Encoder output shape: (batch size, sequence length, units) {sample_output.shape}')
     print(f'Encoder Hidden state shape: (batch size, units) {sample_hidden[0].shape}')
 
-    decoder = DecoderWA(vocab_tar_size, embedding_dim, n_layers, units, BATCH_SIZE)
-    sample_decoder_output, _ = decoder(tf.random.uniform((BATCH_SIZE, 1)),
+    decoder = Decoder(vocab_tar_size, embedding_dim, n_layers, units, BATCH_SIZE)
+    sample_decoder_output, _, _ = decoder(tf.random.uniform((BATCH_SIZE, 1)),
                                           sample_hidden, sample_output)
 
     print(f'Decoder output shape: (batch_size, vocab size) {sample_decoder_output.shape}')
+
+    attention_layer = BahdanauAttention(10)
+    attention_result, attention_weights = attention_layer(sample_hidden[-1], sample_output)
+
+    print(f"Attention result shape: (batch size, units) {attention_result.shape}")
+    print(f"Attention weights shape: (batch_size, sequence_length, 1) {attention_weights.shape}")
 
     optimizer = tf.keras.optimizers.Adam()
     loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
@@ -364,6 +424,8 @@ def _main():
                                     , encoder=encoder
                                     , decoder=decoder)
 
+    input()
+
     evaluate(input_tensor_val
              , target_tensor_val
              , encoder
@@ -372,37 +434,21 @@ def _main():
              , targ_lang
              , max_length_inp
              , max_length_targ
-             , units)
+             , units
+             , n_layers)
 
-    EPOCHS = 10
-
-    for epoch in range(EPOCHS):
-        start = time.time()
-
-        enc_hidden = encoder.initialize_hidden_state()
-        total_loss = 0
-
-        for (batch, (inp, targ)) in enumerate(train_data.take(steps_per_epoch)):
-            batch_loss = train_step( inp
-                                   , targ
-                                   , enc_hidden
-                                   , targ_lang
-                                   , encoder
-                                   , decoder
-                                   , loss_object
-                                   , optimizer
-                                   , BATCH_SIZE)
-            total_loss += batch_loss
-
-            if batch % 100 == 0:
-                print(f'Epoch {epoch + 1} Batch {batch} Loss {batch_loss.numpy():.4f}')
-
-            # saving (checkpoint) the model every 2 epochs
-            if (epoch + 1) % 2 == 0:
-                checkpoint.save(file_prefix=checkpoint_prefix)
-
-        print(f'Epoch {epoch + 1} Loss {total_loss / steps_per_epoch:.4f}')
-        print(f'Time taken for 1 epoch {time.time() - start} sec\n')
+    EPOCHS = 40
+    train( train_data
+         , encoder
+         , decoder
+         , loss_object
+         , optimizer
+         , EPOCHS
+         , BATCH_SIZE
+         , steps_per_epoch
+         , targ_lang
+         , checkpoint
+         , checkpoint_prefix)
 
     translate(u'hace mucho frio aqui.'
              , encoder
