227964b7e64187bf9b27c5bdec340a7a912bdfd5
gortibaldik <ferotre@gmail.com>
2021-03-15 17:56:26 +0100

    Allow selection of custom data dir

--- learning/structured_seq2seq/data_loading.py
+++ learning/structured_seq2seq/data_loading.py
@@ -3,29 +3,36 @@ import statistics
 import numpy as np
 import tensorflow as tf
 
+'''
+MODEL_SET_DATA_DIR needs to be defined before first use of Enum ModelSet,
+then the valid data could be found, otherwise the behavior is undefined
+'''
+MODEL_SET_DATA_DIR = "/content/drive/My Drive/sequence_aware"
+
 class ModelSet(Enum):
-    DATA_DIR = "processed_data"
-    train = [DATA_DIR + '/train/train.summary.id',
-             DATA_DIR + '/train/train.box.val.id',
-             DATA_DIR + '/train/train.box.lab.id',
-             DATA_DIR + '/train/train.box.pos',
-             DATA_DIR + '/train/train.box.rpos',
-             DATA_DIR + '/train/train.box.val',
-             DATA_DIR + '/train/train.box.lab']
-    test =  [DATA_DIR + '/test/test.summary.id',
-             DATA_DIR + '/test/test.box.val.id',
-             DATA_DIR + '/test/test.box.lab.id',
-             DATA_DIR + '/test/test.box.pos',
-             DATA_DIR + '/test/test.box.rpos',
-             DATA_DIR + '/test/test.box.val',
-             DATA_DIR + '/test/test.box.lab' ]
-    val =   [DATA_DIR + '/valid/valid.summary.id',
-             DATA_DIR + '/valid/valid.box.val.id',
-             DATA_DIR + '/valid/valid.box.lab.id',
-             DATA_DIR + '/valid/valid.box.pos',
-             DATA_DIR + '/valid/valid.box.rpos',
-             DATA_DIR + '/valid/valid.box.val',
-             DATA_DIR + '/valid/valid.box.lab']
+    DATA_DIR = "/content/drive/My Drive/sequence_aware"
+    train = [MODEL_SET_DATA_DIR + '/train/train.summary.id',
+             MODEL_SET_DATA_DIR + '/train/train.box.val.id',
+             MODEL_SET_DATA_DIR + '/train/train.box.lab.id',
+             MODEL_SET_DATA_DIR + '/train/train.box.pos',
+             MODEL_SET_DATA_DIR + '/train/train.box.rpos',
+             MODEL_SET_DATA_DIR + '/train/train.box.val',
+             MODEL_SET_DATA_DIR + '/train/train.box.lab']
+    test =  [MODEL_SET_DATA_DIR + '/test/test.summary.id',
+             MODEL_SET_DATA_DIR + '/test/test.box.val.id',
+             MODEL_SET_DATA_DIR + '/test/test.box.lab.id',
+             MODEL_SET_DATA_DIR + '/test/test.box.pos',
+             MODEL_SET_DATA_DIR + '/test/test.box.rpos',
+             MODEL_SET_DATA_DIR + '/test/test.box.val',
+             MODEL_SET_DATA_DIR + '/test/test.box.lab' ]
+    val =   [MODEL_SET_DATA_DIR + '/valid/valid.summary.id',
+             MODEL_SET_DATA_DIR + '/valid/valid.box.val.id',
+             MODEL_SET_DATA_DIR + '/valid/valid.box.lab.id',
+             MODEL_SET_DATA_DIR + '/valid/valid.box.pos',
+             MODEL_SET_DATA_DIR + '/valid/valid.box.rpos',
+             MODEL_SET_DATA_DIR + '/valid/valid.box.val',
+             MODEL_SET_DATA_DIR + '/valid/valid.box.lab']
+
 
 def load_dataset(model_set, batch_size):
     summary_path, text_path, field_path, pos_path, rpos_path, vals_path, labs_path = model_set.value
@@ -45,10 +52,8 @@ def load_dataset(model_set, batch_size):
          tf.convert_to_tensor(poses),
          tf.convert_to_tensor(rposes))
     ).shuffle(BUFFER_SIZE)
-    data = data.batch(batch_size)
+    data = data.batch(batch_size, drop_remainder=True)
     return data
-
-
 
 
 if __name__ == "__main__":
