3928b48a5da5b3aef011286cf9d36c1cf4b52cf9
gortibaldik <ferotre@gmail.com>
2021-03-17 20:41:45 +0100

    Minor bug fixes

--- learning/structured_seq2seq/training.py
+++ learning/structured_seq2seq/training.py
@@ -82,4 +82,4 @@ def train( dataset
 
         # saving the model every epoch
         checkpoint.save(file_prefix=checkpoint_prefix)
-        print(f"Epoch {num} duration : {time.time() - start}")
+        print(f"Epoch {epoch} duration : {time.time() - start}")
