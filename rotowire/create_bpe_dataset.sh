a87b2999a0f9102b775d976e2a4a1e899547b2e2
gortibaldik <ferotre@gmail.com>
2021-04-05 14:47:32 +0200

    Rename, better extraction

--- rotowire/create_bpe_dataset.sh
+++ rotowire/create_bpe_dataset.sh
@@ -1,61 +1,85 @@
 set -e
 
+print_info() {
+  file=$1
+  echo -n "total number of tokens: "
+  wc -l "${file}" | cut -f1 -d' '
+  echo -n "number of tokens with more than five occurrences: "
+  grep -nh '5$' "${file}" | tail -1 | cut -f1 -d:
+}
+
 rotowire_dir=$3
 out_dir=$2
 num_merges=$1
 
 DELETE_VOCAB=0
-# if dictionaries weren't created yet, create them
-if [ ! -f "${out_dir}/all_pfbpe_vocab_${num_merges}.txt" ]; then
-  echo "vocabs weren't created yet, gonna create them in ${out_dir}"
-  ./create_bpe_vocab.sh "${num_merges}" "${out_dir}" "${rotowire_dir}"
+# if codes for bpe weren't learnt yet
+if [ ! -f "${out_dir}/codes_train_${num_merges}.txt" ]; then
+  echo "codes weren't learnt yet, gonna create them in ${out_dir}"
+  ./learn_bpe_codes.sh "${num_merges}" "${out_dir}" "${rotowire_dir}"
   DELETE_VOCAB=1
   echo -e "---\n"
 fi
 
 # pfa means << Prepared For Application of byte pair encoding >>
 # creates ${out_dir}/{train, valid, test}_pfa.txt if they don't exist yet
-for f in "train" "valid" "test"
+for f in "valid" "test"
 do
   if [ ! -f "${out_dir}/${f}_pfa.txt" ]; then
-    echo "preparing the summaries for bpe application"
-    python3 preprocessing.py "${rotowire_dir}" extract_summaries \
+    echo "preparing the summaries for bpe application (${out_dir}/${f}_pfa.txt)"
+    python3 preprocessing.py "${rotowire_dir}" --only_set="${f}" \
+                                               extract_summaries \
                                                --output_dir="${out_dir}" \
                                                --transform_players \
                                                --prepare_for_bpe_application
-    break
   fi
 done
 
 # create ${out_dir}/{train, valid, test}_prepared.txt
 # files segmented in right way, prepared as the input for neural net
+# also create vocabs
 for f in "train" "valid" "test"
 do
   echo "applying bpe to ${f} dataset"
   subword-nmt apply-bpe -c "${out_dir}/codes_train_${num_merges}.txt" \
                         -i "${out_dir}/${f}_pfa.txt" \
-                        --glossaries="<<<[^>]*>>>" | 
+                        --glossaries "<<<[^>]*>>>" "[0-9]+"| 
                         sed -r 's/<<<([^>]*)>>>/\1/g' > "${out_dir}/${f}_prepared.txt"
+
+  subword-nmt get-vocab -o "${out_dir}/${f}_pfbpe_vocab_${num_merges}.txt" \
+                        -i "${out_dir}/${f}_prepared.txt"
+  print_info "${out_dir}/${f}_pfbpe_vocab_${num_merges}.txt"
 done
+
+echo -e "---\ncreating global vocab"
+cat "${out_dir}/train_prepared.txt" \
+    "${out_dir}/valid_prepared.txt" \
+    "${out_dir}/test_prepared.txt" |
+    subword-nmt get-vocab > "${out_dir}/token_vocab.txt"
+
+print_info "${out_dir}/token_vocab.txt"
 
 # store biggest wc to config
 echo "collecting biggest summary stat"
-python3 word_count.py "${out_dir}/train_prepared.txt" \
-                                       "${out_dir}/valid_prepared.txt" \
-                                       "${out_dir}/test_prepared.txt" >> "${out_dir}/config.txt"
-echo "cleaning"
-# cleaning
-for f in "train" "valid" "test"
-do
-  if [ $DELETE_VOCAB -eq 1 ]; then
-    rm "${out_dir}/${f}_pfbpe_vocab_${num_merges}.txt"
-  fi
-  rm "${out_dir}/${f}_pfa.txt"
-done
+python3 word_count.py --log \
+                      "${out_dir}/train_prepared.txt" \
+                      "${out_dir}/valid_prepared.txt" \
+                      "${out_dir}/test_prepared.txt" >> "${out_dir}/config.txt"
 
-if [ $DELETE_VOCAB -eq 1 ]; then
-  mv "${out_dir}/all_pfbpe_vocab_${num_merges}.txt" \
-     "${out_dir}/token_vocab.txt"
+# create tfrecord dataset
+echo "creating tfrecord dataset"
+if [ ! -d "${out_dir}_tfrecord" ]; then
+  mkdir "${out_dir}_tfrecord"
+  python3 preprocessing.py "${rotowire_dir}" create_dataset \
+                                             --preproc_summaries_dir="${out_dir}" \
+                                             --output_dir="${out_dir}_tfrecord" \
+                                             --to_tfrecord
 fi
 
+echo "cleaning"
+for f in "train" "valid" "test"
+do
+  rm "${out_dir}/${f}_pfa.txt"
+  rm "${out_dir}/${f}_pfbpe_vocab_${num_merges}.txt"
+done
 echo "done"
---
+++ rotowire/learn_bpe_codes.sh
@@ -0,0 +1,56 @@
+set -e
+
+rotowire_dir=$3
+out_dir=$2
+num_merges=$1
+
+if [ ! -d "${rotowire_dir}" ]; then
+  1>&2 echo "directory with .json rotowire files (${rotowire_dir}) doesn't exist, quitting" 
+fi
+
+# pfa means <<<Preapared For Application of byte pair encoding>>>
+# pfbpe mean <<<Prepared For Byte Pair Encoding>>> -- training
+# create directory ${out_dir} and files ${out_dir}/{train, valid, test}_pfa.txt
+# plus file with all the player tokens from concatenation of all the summaries
+# in ${out_dir}/player_vocab.txt
+if [ ! -d "$out_dir" ]; then
+  echo "directory ${out_dir} doesn't exist, creating ${out_dir}"
+  mkdir ${out_dir}
+fi
+
+if [ ! -f "${out_dir}/train_pfa.txt" ]; then
+
+  echo "${out_dir}/train_pfa.txt doesn't exist, it is going to be created!"
+  python3 preprocessing.py "${rotowire_dir}" extract_summaries \
+                                             --output_dir="${out_dir}" \
+                                             --transform_players \
+                                             --prepare_for_bpe_application \
+                                             --entity_vocab_path="${out_dir}/entity_vocab.txt" \
+                                             --cell_vocab_path="${out_dir}/cell_vocab.txt" \
+                                             --config_path="${out_dir}/config.txt"
+  # as the input for learn-bpe the text should be without any
+  # tokens not desirable to be merged, therefore each player_token (which is 
+  # enclosed in <<<player_token>>> after preprocessing) is gonna be removed
+  # also each number token should be removed
+  sed 's/<<<[^>]*>>>//g;s/[0-9]+//g' "${out_dir}/train_pfa.txt" > "${out_dir}/train_pfbpe.txt"
+
+elif [ ! -f "${out_dir}/train_pfbpe.txt" ]; then
+
+  # ${out_dir}/train_pfa.txt must exist
+  sed 's/<<<[^>]*>>>//g;s/[0-9]+//g' "${out_dir}/train_pfa.txt" > "${out_dir}/train_pfbpe.txt"
+fi
+
+# learn bpe codes file in ${out_dir}/codes_train_${num_merges}.txt
+echo "learning bpe!"
+subword-nmt learn-bpe -s "${num_merges}" \
+                      -o "${out_dir}/codes_train_${num_merges}.txt" \
+                      -i "${out_dir}/train_pfbpe.txt"
+
+# cleaning
+rm "${out_dir}/train_pfbpe.txt"
+
+# after the script, files: train_pfa.txt
+#                          codes_train_${num_merges}.txt
+#                          entity_vocab.txt
+#                          cell_vocab.txt
+# should be present in the ${out_dir}
