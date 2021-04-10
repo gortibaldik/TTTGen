fa90348e2a12cf16a7fa59874c950b334a9dfe69
gortibaldik <ferotre@gmail.com>
2021-04-10 14:51:49 +0200

    Decoder as RNN layer to allow iterating with less memory requirements

--- rotowire/baseline_model/layers.py
+++ rotowire/baseline_model/layers.py
@@ -67,4 +67,50 @@ class DotAttention(tf.keras.layers.Layer):
         alignment = tf.nn.softmax(score)
         context = tf.reduce_sum(tf.expand_dims(alignment, -1) * all_encs, axis=1)
         return context, alignment
-        
+
+class DecoderRNNCell(tf.keras.layers.Layer):
+    def __init__( self
+                , word_vocab_size
+                , word_emb_dim
+                , decoder_rnn_dim
+                , batch_size):
+        super(DecoderRNNCell, self).__init__()
+        self._word_emb_dim = word_emb_dim
+        self._embedding = tf.keras.layers.Embedding(word_vocab_size, word_emb_dim)
+        self._rnn_1 = tf.keras.layers.LSTMCell( decoder_rnn_dim
+                                              , recurrent_initializer='glorot_uniform')
+        self._rnn_2 = tf.keras.layers.LSTMCell( decoder_rnn_dim
+                                              , recurrent_initializer='glorot_uniform')
+        self._fc_1 = tf.keras.layers.Dense( decoder_rnn_dim
+                                          , activation='tanh')
+        self._fc_2 = tf.keras.layers.Dense( word_vocab_size
+                                          , activation='softmax')
+        self._attention = DotAttention()
+        self._batch_size = batch_size
+        self._hidden_size = decoder_rnn_dim
+        self._enc_outs = None
+        self.state_size = [ tf.TensorShape([self._hidden_size]),
+                            tf.TensorShape([self._hidden_size]),
+                            tf.TensorShape([self._hidden_size]),
+                            tf.TensorShape([self._hidden_size]),
+                            tf.TensorShape([self._hidden_size])]
+
+    def initialize_enc_outs(self, enc_outs):
+        self._enc_outs = enc_outs
+    
+    def get_last_alignment(self):
+        return self._last_alignment
+
+    def call(self, x, states):
+        emb = self._embedding(x)
+        emb = tf.squeeze(emb)
+        last_hidden_attn, h1, c1, h2, c2 = states
+        emb_att = tf.concat([emb, last_hidden_attn], axis=-1)
+        seq_output, (h1, c1) = self._rnn_1( emb_att, (h1, c1))
+        seq_output, (h2, c2) = self._rnn_2( seq_output, (h2, c2))
+        context, self._last_alignment = self._attention(h2, self._enc_outs)
+        concat_ctxt_h2 = tf.concat([context, h2], axis=-1)
+        hidden_att = self._fc_1(concat_ctxt_h2)
+        result = self._fc_2(hidden_att)
+
+        return result, (hidden_att, h1, c1, h2, c2)
