fbaf3c5dc95a31d321c9fb477d72852824ccdc3b
gortibaldik <ferotre@gmail.com>
2021-04-12 16:36:32 +0200

    fix logs path

--- rotowire/scripts/enqueue_gpu_job.sh
+++ rotowire/scripts/enqueue_gpu_job.sh
@@ -1,9 +1,8 @@
 rm ~/rotowire/logs/gpu_*
 
-qsub -q \
-     gpu.q \
+qsub -q gpu.q \
      -cwd \
-     -o /rotowire/logs/gpu_output.txt \
+     -o ~/rotowire/logs/gpu_output.txt \
      -e ~/rotowire/logs/gpu_errors.txt \
      -pe smp 1 -l \
      gpu=1,mem_free=16G,act_mem_free=16G,h_data=32G \
