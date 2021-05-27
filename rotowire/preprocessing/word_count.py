a6308953609ed2a51c9a7e6503b485d911edd0b6
gortibaldik <ferotre@gmail.com>
2021-05-27 20:27:13 +0200

    DocString wordcount

--- rotowire/preprocessing/word_count.py
+++ rotowire/preprocessing/word_count.py
@@ -4,6 +4,15 @@ from argparse import ArgumentParser
 import sys
 
 def get_most_words_line_count(file_path, log):
+    """ Traverse file and compute various statistics about word-length of its lines
+
+    Args:
+      file_path : path to file
+      log:  whether to show minimal, maximal and average length of lines in the file
+    
+    Returns:
+      maximal length of line in the file
+    """
     with open(file_path, 'r') as f:
         content = f.read().strip().split('\n')
     _max = 0
@@ -19,11 +28,13 @@ def get_most_words_line_count(file_path, log):
     return _max
 
 if __name__ == "__main__":
-  parser = ArgumentParser(
-    description='Print length of the line with most words from all the files provided'
-  )
-  parser.add_argument('file_names', metavar='FILE_NAME', type=str, nargs='+')
-  parser.add_argument('--log', help='print stats of individual files', action='store_true')
+  parser = ArgumentParser( description='Print length of the line with most words from all the files provided')
+  parser.add_argument( 'file_names'
+                     , metavar='FILE_NAME'
+                     , type=str, nargs='+')
+  parser.add_argument( '--log'
+                     , help='print stats of individual files'
+                     , action='store_true')
   args = vars(parser.parse_args())
 
   wc = 0
