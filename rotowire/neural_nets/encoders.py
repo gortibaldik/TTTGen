0b3df1af3ffd0c0aa1adb49de35479ef9ee85410
gortibaldik <ferotre@gmail.com>
2021-05-27 14:50:49 +0200

    Encoders documentation strings

--- rotowire/neural_nets/encoders.py
+++ rotowire/neural_nets/encoders.py
@@ -2,6 +2,8 @@ import tensorflow as tf
 from neural_nets.layers import Embedding, MLPEncodingCell, ContentSelectionCell
 
 class Encoder(tf.keras.Model):
+    """ Baseline Encoder as described in section 4.2.1 of the thesis
+    """
     def __init__( self
                 , word_vocab_size
                 , word_emb_dim
@@ -12,6 +14,20 @@ class Encoder(tf.keras.Model):
                 , entity_span
                 , hidden_size
                 , batch_size):
+        """ Initialize Encoder
+
+        Args:
+            word_vocab_size         (int):      size of the vocabulary of words
+            word_emb_dim            (int):      embedding dimensionality to which project the words from the summary and value and entity
+                                                part of the input records
+            tp_vocab_size           (int):      size of the vocabulary of types
+            tp_emb_dim              (int):      embedding dimensionality to which project the type part of input record
+            ha_vocab_size           (int):      size of the vocabulary of home/away flags
+            ha_emb_dim              (int):      embedding dimensionality to which project the home/away flag part of input record
+            entity_span             (int):      number of records belonging to one player entity
+            hidden_size             (int):      dimensionality of the hidden states
+            batch_size              (int):      the size of batches in the dataset
+        """
         super(Encoder, self).__init__()
         self._embedding = Embedding( word_vocab_size
                                    , word_emb_dim
@@ -32,16 +48,27 @@ class Encoder(tf.keras.Model):
         self._batch_size = new_batch_size
 
     def call(self, inputs):
+        """ inputs should be organized as (types, entities, values, home/away flags)"""
+        # embed the records
         embedded = self._embedding(inputs)
         embedded = tf.concat(embedded, axis=2) # pylint: disable=unexpected-keyword-arg, no-value-for-parameter
+
+        # pass through mlp
         all_states, _ = self._rnn(embedded)
+
+        # prepare states for the decoder
         pooled = self._pooling(all_states)
         pooled = tf.reshape(pooled, [self._batch_size, -1])
         trans = self._linear_transform(pooled)
         hidden_1, cell_1, hidden_2, cell_2 = tf.split(trans, 4 , 1)
+
         return all_states, hidden_1, cell_1, hidden_2, cell_2
 
 class EncoderCS(tf.keras.Model):
+    """ Encoder with Content Selection mechanism as described in section 4.4.1 in the thesis
+
+    Uses self-attention to model context awareness of the representation of the records to other records.
+    """
     def __init__( self
                 , word_vocab_size
                 , word_emb_dim
@@ -53,6 +80,21 @@ class EncoderCS(tf.keras.Model):
                 , hidden_size
                 , attention_type
                 , batch_size):
+        """ Initialize EncoderCS
+
+        Args:
+            word_vocab_size         (int):      size of the vocabulary of words
+            word_emb_dim            (int):      embedding dimensionality to which project the words from the summary and value and entity
+                                                part of the input records
+            tp_vocab_size           (int):      size of the vocabulary of types
+            tp_emb_dim              (int):      embedding dimensionality to which project the type part of input record
+            ha_vocab_size           (int):      size of the vocabulary of home/away flags
+            ha_emb_dim              (int):      embedding dimensionality to which project the home/away flag part of input record
+            max_seq_size            (int):      maximal length of the records from the dataset
+            attention_type          (callable): method which initializes the attention mechanism
+            hidden_size             (int):      dimensionality of the hidden states
+            batch_size              (int):      the size of batches in the dataset
+        """
         super(EncoderCS, self).__init__()
         self._embedding = Embedding( word_vocab_size
                                    , word_emb_dim
@@ -73,14 +115,24 @@ class EncoderCS(tf.keras.Model):
         self._batch_size = new_batch_size
 
     def call(self, inputs):
+        """ inputs should be organized as (types, entities, values, home/away flags)"""
+        # embed the records
         embedded = self._embedding(inputs)
         embedded = tf.concat(embedded, axis=2) # pylint: disable=unexpected-keyword-arg, no-value-for-parameter
+
+        # pass through mlp
         all_states, _ = self._rnn(embedded)
+
+        # content selection 
         content_selected, *_ = self._cs_rnn(all_states, initial_state=(all_states, tf.zeros((), dtype=tf.int32)))
         avg = tf.reduce_mean(content_selected, axis=1)
         return content_selected, avg, avg, avg, avg
 
 class EncoderCSBi(tf.keras.Model):
+    """ Encoder with Content Selection and Bidirectional LSTM layer (encoder from CopyCSBidir model) as described in section 4.4.2 in the thesis
+
+    A wrapper around EncoderCS with additional bidirectional LSTM layer at the top of the outputs of EncoderCS
+    """
     def __init__( self
                 , word_vocab_size
                 , word_emb_dim
@@ -93,6 +145,24 @@ class EncoderCSBi(tf.keras.Model):
                 , attention_type
                 , batch_size
                 , dropout_rate):
+        """ Initialize EncoderCSBi
+
+        Args:
+            word_vocab_size         (int):      size of the vocabulary of words
+            word_emb_dim            (int):      embedding dimensionality to which project the words from the summary and value and entity
+                                                part of the input records
+            tp_vocab_size           (int):      size of the vocabulary of types
+            tp_emb_dim              (int):      embedding dimensionality to which project the type part of input record
+            ha_vocab_size           (int):      size of the vocabulary of home/away flags
+            ha_emb_dim              (int):      embedding dimensionality to which project the home/away flag part of input record
+            max_seq_size            (int):      maximal length of the records from the dataset
+            hidden_size             (int):      dimensionality of the hidden states
+            attention_type          (callable): method which initializes the attention mechanism
+            batch_size              (int):      the size of batches in the dataset
+            dropout_rate            (float):    rate at which to drop cells and corresponding connections at
+                                                the outputs of the internal LSTM layers of the model (number
+                                                between 0 and 1, 0 means no dropout)
+        """
         super(EncoderCSBi, self).__init__()
         self._encoder_cs = EncoderCS( word_vocab_size
                                     , word_emb_dim
