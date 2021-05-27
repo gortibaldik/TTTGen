30427751172cdebb1faf473f507f670f4e90c0bd
gortibaldik <ferotre@gmail.com>
2021-05-27 22:38:02 +0200

    DocString extract_matches

--- rotowire/preprocessing/extract_matches.py
+++ rotowire/preprocessing/extract_matches.py
@@ -6,6 +6,7 @@ except:
     from .match_stat_class import MatchStat
 
 def extract_matches_from_json(json_file_path, **kwargs):
+    """ Extract dict from json and traverse all the mentioned matches """
     matches = []
     with open(json_file_path, 'r', encoding='utf8') as f:
         for match in json.load(f, object_pairs_hook=OrderedDict):
