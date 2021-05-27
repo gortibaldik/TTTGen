ae0b287f8c0d5168016dab2263ab2ecb2c2b8479
gortibaldik <ferotre@gmail.com>
2021-05-27 16:21:16 +0200

    DocStrings baseline_model

--- rotowire/neural_nets/baseline_model.py
+++ rotowire/neural_nets/baseline_model.py
@@ -5,19 +5,42 @@ import sys
 from neural_nets.layers import DecoderRNNCellJointCopy
 
 class EncoderDecoderBasic(tf.keras.Model):
+    """ EncoderDecoder model which allows usage of different Encoders (Encoder, EncoderCS, EncoderCSBi) and Decoders(DecoderRNNCell, DecoderRNNCellJointCopy)
+
+    Encoder should encode the input records to a representation out of which the decoder would generate (decode) the output text
+    """
     def __init__( self
                 , encoder
                 , decoder_cell):
+        """ Initialize EncoderDecoderBasic
+        Args:
+            encoder:        one of Encoder, EncoderCS, EncoderCSBi
+            decoder_cell:   one of DecoderRNNCell, DecoderRNNCellJointCopy
+        """
         super(EncoderDecoderBasic, self).__init__()
         self._encoder = encoder
         self._decoder_cell = decoder_cell
 
-    def compile(self
+    def compile( self
                , optimizer
                , loss_fn
                , scheduled_sampling_rate
                , truncation_size
                , truncation_skip_step):
+        """ Prepare the model for training, evaluation and prediction
+
+        Assigns optimizers, losses, initiates training hyperparameters, sets up eager execution,
+        which enables us to use different settings for training (we use graph execution during training)
+        and evaluation and prediction (where we use eager execution)
+
+            Args:
+            optimizer               (optimizer):    optimizer used to minimize the txt loss
+            loss_fn                 (loss):         loss function
+            scheduled_sampling_rate (float):        frequency at which the gold outputs from the previous time-steps are fed into the network
+                                                    (number between 0 and 1, 1 means regular training)
+            truncation_size         (int):          t_2 argument of TBPTT (explained in section 4.1 of the thesis)
+            truncation_skip_step    (int):          t_1 argument of TBPTT (should be lower than or equal to t_2)
+        """
         super(EncoderDecoderBasic, self).compile(run_eagerly=True)
         self._optimizer = optimizer
         self._loss_fn = loss_fn
@@ -31,7 +54,7 @@ class EncoderDecoderBasic(tf.keras.Model):
         self._generator = tf.random.Generator.from_non_deterministic_state()
 
     def _calc_loss( self, x, y):
-        """ use only the non-pad values """
+        """ use only the non-pad values to calculate the loss"""
         mask = tf.math.logical_not(tf.math.equal(y, 0))
         loss_ = self._loss_fn(y, x)
         mask = tf.cast(mask, loss_.dtype)
@@ -45,6 +68,26 @@ class EncoderDecoderBasic(tf.keras.Model):
                  , batch_data
                  , last_out
                  , initial_state=None):
+        """ performs one forward and backward pass
+
+        the first pass with given arguments creates a computational graph which 
+        is used in the other passes
+
+        Args:
+            batch_data:     inputs and targets for the text decoder, input tables, and
+                            generated probabilities for scheduled sampling
+                            (dec_in, dec_targets, gen_or_teach, *tables)
+            last_out:       last output of the decoder
+            initial_state:  initial state for the decoder
+        
+        Returns:
+            self._truncation_skip_step-th hidden state
+            argmax of self._truncation_skip_step-th prediction of the network
+        """
+
+        # debugging outputs - tf.function calls python functions only 
+        # during tracing when a computation graph is created
+        # we report how many times the function is traced and with which arguments
         print("tracing tf.function with args:", file=sys.stderr)
         print(f"len(batch_data) : {len(batch_data)}", file=sys.stderr)
         for ix, d in enumerate(batch_data):
@@ -56,6 +99,7 @@ class EncoderDecoderBasic(tf.keras.Model):
         else:
           for ix, d in enumerate(initial_state):
             print(f"initial_state[{ix}].shape : {d.shape}", file=sys.stderr)
+        
         loss = 0
         dec_in, targets, gen_or_teach, *tables = batch_data
         final_state = None
@@ -65,7 +109,7 @@ class EncoderDecoderBasic(tf.keras.Model):
             if initial_state is None:
                 initial_state = [ last_hidden_rnn[-1]
                                 , *last_hidden_rnn ]
-
+            # prepare states and inputs for the decoder
             if isinstance(self._decoder_cell, DecoderRNNCellJointCopy):
                 print("using joint copy mechanism !")
                 enc_ins = tf.one_hot(tf.cast(tables[2], tf.int32), self._decoder_cell._word_vocab_size) # pylint: disable=unexpected-keyword-arg, no-value-for-parameter
@@ -87,11 +131,15 @@ class EncoderDecoderBasic(tf.keras.Model):
                 last_out, states = self._decoder_cell( (_input, *aux_inputs)
                                                      , states=states
                                                      , training=True)
+                # calculate loss and collect metrics
                 loss += self._calc_loss( last_out
                                        , targets[:, t])
+                # prepare new input for the decoder (used with (1 - scheduled_sampling_rate) probability)
                 last_out = tf.expand_dims(tf.cast(tf.argmax(last_out, axis=1), tf.int16), -1)
 
         variables = []
+        # linear transformation layer is trained only when the initial_state is None
+        # when it prepares the initial states for the decoder
         for var in self._encoder.trainable_variables + self._decoder_cell.trainable_variables:
             if (initial_state is None) or (var.name != 'encoder/linear_transform/kernel:0'):
                 variables.append(var)
@@ -108,24 +156,44 @@ class EncoderDecoderBasic(tf.keras.Model):
 
     @property
     def metrics(self):
+        """returns the list of metrics that should be reset at the start and end of training epoch and evaluation"""
         return self._train_metrics + list(self._val_metrics.values())
 
 
     def train_step(self, batch_data):
+        """ perform one train_step during model.fit
+
+        Args:
+            batch_data: data to train on in format (summaries, *tables)
+        """
         summaries, *tables = batch_data
+
+        # prepare inputs for the decoder
         sums = tf.expand_dims(summaries, axis=-1)
+
+        # scheduled sampling may force the text decoder to generate
+        # from its last prediction even at the first step
+        # by setting last_out = sums[:, 0] we erase differences between
+        # scheduled sampling and teacher forcing at the first timestep
         last_out = sums[:, 0]
         start = 0
         length = summaries.shape[1]
         state = None
         for end in range(self._truncation_size, length-1, self._truncation_skip_step):
+            # gen_or_teach is the [0,1] vector which contains value for each time-step
+            # if the value for the timestep is higher than self.scheduled_sampling_rate
+            # the text decoder is forced to generate from its last prediction
             gen_or_teach = np.zeros(shape=(end-start))
             for i in range(len(gen_or_teach)):
                 gen_or_teach[i] = self._generator.uniform(shape=(), maxval=1.0)
+
+            # prepare data for teacher forcing, scheduled sampling etc.
             truncated_data = ( sums[:, start:end, :]
                              , summaries[:, start+1:end+1]
                              , tf.convert_to_tensor(gen_or_teach)
                              , *tables)
+            
+            # run the backpropagation on truncated sequence
             state, last_out =  self.bppt_step( truncated_data
                                              , last_out
                                              , initial_state=state)
@@ -133,13 +201,20 @@ class EncoderDecoderBasic(tf.keras.Model):
         # finish the truncated bppt if the truncation_size cannot divide properly
         # the length of sequence
         if (length - self._truncation_size) % self._truncation_skip_step != 0:
+            # gen_or_teach is the [0,1] vector which contains value for each time-step
+            # if the value for the timestep is higher than self.scheduled_sampling_rate
+            # the text decoder is forced to generate from its last prediction
             gen_or_teach = np.zeros(shape=(length-1-start))
             for i in range(len(gen_or_teach)):
                 gen_or_teach[i] = self._generator.uniform(shape=(), maxval=1.0)
+
+            # prepare data for teacher forcing, scheduled sampling etc.
             truncated_data = ( sums[:, start:length-1, :]
                                 , summaries[:, start+1:length]
                                 , tf.convert_to_tensor(gen_or_teach)
                                 , *tables)
+
+            # run the backpropagation on truncated sequence
             state, last_out = self.bppt_step( truncated_data
                                             , last_out
                                             , initial_state=state)
@@ -147,13 +222,20 @@ class EncoderDecoderBasic(tf.keras.Model):
 
 
     def test_step(self, batch_data):
+        """ perform one test_step during model.evaluate
+
+        Args:
+            batch_data: data to train on in format (summaries, *tables)
+        """
         summaries, *tables = batch_data
+        # prepare summaries
         max_sum_size = summaries.shape[1] - 1
         dec_inputs = tf.expand_dims(summaries, axis=-1)[:, :max_sum_size, :]
         targets = summaries[:, 1:max_sum_size+1]
 
         enc_outs, *last_hidden_rnn = self._encoder(tables, training=False)
 
+        # prepare states and inputs for the decoder
         if isinstance(self._decoder_cell, DecoderRNNCellJointCopy):
             enc_ins = tf.one_hot(tf.cast(tables[2], tf.int32), self._decoder_cell._word_vocab_size) # pylint: disable=unexpected-keyword-arg, no-value-for-parameter
             aux_inputs = (enc_outs, enc_ins) # value portion of the record needs to be copied
@@ -165,6 +247,7 @@ class EncoderDecoderBasic(tf.keras.Model):
 
         result_preds = np.zeros(targets.shape, dtype=np.int)
 
+        # decode text
         for t in range(targets.shape[1]):
             pred, initial_state = self._decoder_cell( (dec_in, *aux_inputs)
                                                     , initial_state
@@ -187,6 +270,11 @@ class EncoderDecoderBasic(tf.keras.Model):
         return dict([(metric.name, metric.result()) for metric in self._val_metrics.values()])
 
     def predict_step(self, data):
+        """ perform one predict_step during model.predict
+
+        Args:
+            batch_data: data to train on in format (summaries, *tables)
+        """
         summaries, *tables = data
 
         # retrieve start tokens
@@ -205,6 +293,7 @@ class EncoderDecoderBasic(tf.keras.Model):
 
         result_preds = np.zeros(summaries.shape, dtype=np.int)
 
+        # greedy decoding
         for t in range(summaries.shape[1]):
             pred, initial_state = self._decoder_cell( (dec_in, *aux_inputs)
                                                     , initial_state
@@ -215,107 +304,3 @@ class EncoderDecoderBasic(tf.keras.Model):
             dec_in = tf.expand_dims(predicted_ids, axis=1)
 
         return result_preds
-
-
-class BeamSearchAdapter(tf.keras.Model):
-    def __init__( self
-                , encoder_decoder_basic : EncoderDecoderBasic
-                , beam_size
-                , eos):
-        super(BeamSearchAdapter, self).__init__()
-        self._encoder = encoder_decoder_basic._encoder
-        self._decoder_cell = encoder_decoder_basic._decoder_cell
-        self._beam_size = beam_size
-        self._eos = eos
-
-    def compile(self):
-        super(BeamSearchAdapter, self).compile(run_eagerly=True)
-
-    def call(self, batch_data):
-        summaries, *tables = batch_data
-
-        # retrieve start tokens
-        dec_inputs = tf.expand_dims(summaries, axis=-1)
-        dec_in = dec_inputs[:, 0, :] # start tokens
-
-        enc_outs, *last_hidden_rnn = self._encoder(tables, training=False)
-
-        if isinstance(self._decoder_cell, DecoderRNNCellJointCopy):
-            enc_ins = tf.one_hot(tf.cast(tables[2], tf.int32), self._decoder_cell._word_vocab_size) # pylint: disable=unexpected-keyword-arg, no-value-for-parameter
-            aux_inputs = (enc_outs, enc_ins) # value portion of the record needs to be copied
-        else:
-            aux_inputs = (enc_outs,)
-
-        initial_state = [last_hidden_rnn[-1], *last_hidden_rnn]
-
-        actual_beam_size = self._beam_size
-        hypotheses = []
-        mask = tf.ones(shape=dec_in.shape, dtype=tf.bool)
-        hypotheses.append((dec_in, mask, tf.zeros(shape=summaries.shape[0]), initial_state))
-
-        out_sentence = tf.TensorArray(size=summaries.shape[1], dtype=tf.int32)
-        out_predecessors = tf.TensorArray(size=summaries.shape[1], dtype=tf.int32)
-        for t in range(summaries.shape[1]):
-            bs_sqrd = len(hypotheses) * actual_beam_size
-            scores = tf.TensorArray(size=bs_sqrd, dtype=tf.float32)
-            indices = tf.TensorArray(size=bs_sqrd, dtype=tf.int32)
-            prdcsrs = tf.TensorArray(size=bs_sqrd, dtype=tf.int32)
-            initial_states = []
-            masks = []
-            siix = 0
-            for h in range(len(hypotheses)):
-                dec_in, mask, nlogsum, initial_state = hypotheses.pop(0)
-                mask = tf.math.logical_not(tf.math.logical_or(tf.math.logical_not(mask) , dec_in == self._eos))
-                pred, (hatt, h1, c1, h2, c2) = self._decoder_cell( (dec_in, *aux_inputs)
-                                                                 , initial_state
-                                                                 , training=False)
-                top_predicted = tf.math.top_k(pred, k=actual_beam_size, sorted=True)
-                ixs = top_predicted.indices * tf.cast(mask, dtype=tf.int32) # pylint: disable=no-value-for-parameter, unexpected-keyword-arg
-                vals = tf.math.log(top_predicted.values) * tf.cast(mask, dtype=tf.float32) # pylint: disable=no-value-for-parameter, unexpected-keyword-arg
-
-                # collect batch states and masks
-                batch_states = []
-                mm = []
-                for i in range(dec_in.shape[0]):
-                    batch_states.append((hatt[i], h1[i], c1[i], h2[i], c2[i]))
-                    mm.append(mask[i])
-                initial_states.append(batch_states)
-                masks.append(mm)
-
-                for top in range(ixs.shape[1]):
-                    scores = scores.write(siix, vals[:, top] + nlogsum)
-                    indices = indices.write(siix, ixs[:, top])
-                    prdcsrs = prdcsrs.write(siix, tf.ones(shape=ixs[:, top].shape, dtype=tf.int32) * h)
-                    siix += 1
-            scores = tf.transpose(scores.stack(), [1, 0])
-            indices = tf.transpose(indices.stack(), [1, 0])
-            prdcsrs = tf.transpose(prdcsrs.stack(), [1, 0])
-
-            top_k_scores = tf.math.top_k(scores, k=actual_beam_size)
-            top_k_indices = tf.gather(indices, top_k_scores.indices, batch_dims=-1) # pylint: disable=no-value-for-parameter
-            out_sentence = out_sentence.write(t, top_k_indices)
-            top_k_prdcsrs = tf.gather(prdcsrs, top_k_scores.indices, batch_dims=-1) # pylint: disable=no-value-for-parameter
-            out_predecessors = out_predecessors.write(t, top_k_prdcsrs)
-            for beam_ix in range(actual_beam_size):
-                next_in = tf.expand_dims(top_k_indices[:, beam_ix], -1)
-                for batch_ix in range(summaries.shape[0]):
-                    if batch_ix == 0:
-                        hatt, h1, c1, h2, c2 = initial_states[top_k_prdcsrs[batch_ix, beam_ix].numpy()][batch_ix]
-                        f = lambda x: tf.expand_dims(x, 0)
-                        hatt, h1, c1, h2, c2 = f(hatt), f(h1), f(c1), f(h2), f(c2)
-                        mask = f(masks[top_k_prdcsrs[batch_ix, beam_ix].numpy()][batch_ix])
-                    else:
-                        hatt2, h12, c12, h22, c22 = initial_states[top_k_prdcsrs[batch_ix, beam_ix].numpy()][batch_ix]
-                        f = lambda x, y: tf.concat([x, tf.expand_dims(y, 0)], axis=0) # pylint: disable=unexpected-keyword-arg, no-value-for-parameter
-                        hatt, h1, c1, h2, c2 = f(hatt, hatt2), f(h1, h12), f(c1, c12), f(h2, h22), f(c2, c22)
-                        mask = f(mask, masks[top_k_prdcsrs[batch_ix, beam_ix].numpy()][batch_ix])
-                i_s = (hatt, h1, c1, h2, c2)
-                hypotheses.append((next_in, mask, top_k_scores.values[:, beam_ix], i_s))
-
-        out_predecessors = out_predecessors.stack()
-        out_sentence = out_sentence.stack()
-
-        out_sentence = tf.transpose(out_sentence, [1, 0, 2])
-        out_predecessors = tf.transpose(out_predecessors, [1, 0, 2])
-
-        return out_sentence, out_predecessors
